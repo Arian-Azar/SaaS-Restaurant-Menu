91e7aa4983b338681adde83ef0a9ab4e627e000b
Arian Azarnioshe <ariangithub@gmail.com>
2026-09-06 20:30:04 +0330

    Update base settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -155,3 +155,11 @@ REST_FRAMEWORK = {
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# -----------------------------------------------------------------------
+# Authentication redirects
+# -----------------------------------------------------------------------
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGOUT_REDIRECT_URL = 'login'
